f2c801225dba952e288a448b2aef576c12f7d38c
bo0kproject <143459131+bo0kproject@users.noreply.github.com>
2023-09-07 17:53:27 +0400

    Update 1.js

--- 1.js
+++ 1.js
@@ -1,69 +1,69 @@
 // создание вопросов и ответов
 let questions = [
+    {
+    numb: 1,
+    question: "Azərbaycan Respublikasında gəlir vergisinin ödəyicisi kimlərdir?",
+    answer: "rezident və qeyri-rezident fiziki şəxslər",
+    options: [
+      "yalnız rezident fiziki şəxslər",
+      "yalnız qeyri-rezident fiziki şəxslər",
+      "rezident və qeyri-rezident fiziki şəxslər",
+      "fərdi sahibkarlar"
+    ]
+  },
+    {
+    numb: 2,
+    question: "Вопрос номер 2?",
+    answer: "1",
+    options: [
+      "1",
+      "2",
+      "3",
+      "4"
+    ]
+  },
+    {
+    numb: 3,
+    question: "Вопрос номер 3?",
+    answer: "1",
+    options: [
+      "1",
+      "2",
+      "3",
+      "4"
+    ]
+  },
+    {
+    numb: 4,
+    question: "Вопрос номер 4?",
+    answer: "1",
+    options: [
+      "1",
+      "2",
+      "3",
+      "4"
+    ]
+  },
+    {
+    numb: 5,
+    question: "Вопрос номер 5?",
+    answer: "1",
+    options: [
+      "1",
+      "2",
+      "3",
+      "4"
+    ]
+  },
   {
-  numb: 1,
-  question: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ullamcorper felis non mollis vestibulum. Nunc a eleifend libero. Aliquam mollis, nisl sit amet lobortis euismod, nunc dui finibus sem, vel fringilla ligula ex eget sem. Interdum et malesuada fames ac ante ipsum primis in faucibus. Aenean non mi quam. Sed auctor leo quis diam fermentum, et laoreet purus bibendum. Fusce lobortis sapien sed nunc condimentum, et ornare justo fermentum. Praesent ipsum risus, sollicitudin lacinia venenatis non, scelerisque ut est. Sed vestibulum lectus eget lacus dignissim vehicula.?",
-  answer: "1",
-  options: [
-    "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
-    "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
-    "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
-    "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
-  ]
-},
-  {
-  numb: 2,
-  question: "Вопрос номер 1?",
-  answer: "1",
-  options: [
-    "1",
-    "2",
-    "3",
-    "4"
-  ]
-},
-  {
-  numb: 3,
-  question: "Вопрос номер 3?",
-  answer: "1",
-  options: [
-    "1",
-    "2",
-    "3",
-    "4"
-  ]
-},
-  {
-  numb: 4,
-  question: "Вопрос номер 4?",
-  answer: "1",
-  options: [
-    "1",
-    "2",
-    "3",
-    "4"
-  ]
-},
-  {
-  numb: 5,
-  question: "Вопрос номер 5?",
-  answer: "1",
-  options: [
-    "1",
-    "2",
-    "3",
-    "4"
-  ]
-},
-{
-  numb: 6,
-  question: "Вопрос номер 6?",
-  answer: "1",
-  options: [
-    "1",
-    "2",
-    "3",
-    "4"
-  ]
-},
-];
+    numb: 6,
+    question: "Вопрос номер 6?",
+    answer: "1",
+    options: [
+      "1",
+      "2",
+      "3",
+      "4"
+    ]
+  },
+  ];
