29f637d29d48bd4d61abae56697a87f081be3b6e
bo0kproject <143459131+bo0kproject@users.noreply.github.com>
2023-09-08 19:37:10 +0400

    Update 2.js

--- 2.js
+++ 2.js
@@ -2,40 +2,62 @@
 let questions = [
   {
   numb: 1,
-  question: "46.	Muzdlu işçi kimi cəlb etmədiyi fiziki şəxslərə hüquqi şəxslər və ya fərdi sahibkarlar tərəfindən ödənilən maddi yardım, mükafat və təqaüdlərdən gəlir vergisi necə tutulur?",
-  answer: "1",
+  question: "Muzdlu işçi kimi cəlb etmədiyi fiziki şəxslərə hüquqi şəxslər və ya fərdi sahibkarlar tərəfindən ödənilən maddi yardım, mükafat və təqaüdlərdən gəlir vergisi necə tutulur?",
+  answer: "A)	14 faiz",
   options: [
-    "1",
-    "2",
-    "3",
-    "4"
+    "A)	14 faiz",
+    "B)	10 faiz",
+    "C)	3.0000 manat x 14% + 30.0000 manatdan çox olan məbləğin 25 faizi",
+    "D)	10000 manat x 14% + 10000 manatdan çox olan məbləğin 25 faizi"
   ]
 },
   {
   numb: 2,
-  question: "Вопрос номер 2?",
-  answer: "1",
+  question: "Vəkil qurumlarının tərkibində fəaliyyət göstərən vəkillərin həmin qurum tərəfindən ödənilən gəlirlərindən vergi necə tutulur?",
+  answer: "D)	14 faiz",
   options: [
-    "1",
-    "2",
-    "3",
-    "4"
+    "A)	25 faiz",
+    "B)	10 faiz",
+    "C)	3.0000 manat x 14% + 30.0000 manatdan çox olan məbləğin 25 faizi",
+    "D)	14 faiz"
   ]
 },
   {
   numb: 3,
-  question: "Вопрос номер 3?",
-  answer: "1",
+  question: "48.	Hüquqi şəxs yaratmadan sahibkarlıq fəaliyyətini həyata keçirən fiziki şəxslərin vergiyə cəlb edilən gəlirlərindən gəlir vergisi hansı qaydada tutlur?",
+  answer: "A)	20 faiz",
   options: [
-    "1",
-    "2",
-    "3",
-    "4"
+    "A)	20 faiz",
+    "B)	14 faiz",
+    "C)	30.000 manat x 14% + 30.0000 manatdan çox olan məbləğin 25 faizi",
+    "D)	30.000 manatadək vergi tutulmur, 30.000 manatdan yuxan məbləğin 20 faizi"
   ]
 },
   {
   numb: 4,
-  question: "Вопрос номер 4?",
+  question: "49.	Xüsusi notariuslar tərəfindən bir ay ərzində aparılan notariat hərəkətlərinə, habelə notariat hərəkətləri ilə əlaqədar göstərilən xidmətlərə görə alman haqlar hansı qaydada gəlir vergisinə cəlb olunur?",
+  answer: "C) xərclər çıxılmadan 10% dərəcə ilə",
+  options: [
+    "A)	ödəmə mənbəyində 10% dərəcə ilə ",
+    "B)	xərclər çıxıldıqdan sonra 10% dərəcə ilə ",
+    "C) xərclər çıxılmadan 10% dərəcə ilə",
+    "D) xərclər çıxılmadan 14% dərəcə ilə"
+  ]
+},
+  {
+  numb: 5,
+  question: "50.	İdman mərc oyunlarından, lotereyalardan, habelə digər yarışlardan və müsabiqələrdən pul şəklində əldə edilən uduşlardan (mükafatlardan) iştirakla bağlı ödənilən pul vəsaiti (pul qoyuluşu) çıxılmaqla qalan məbləğdən (Azərbaycan Respublikasının hüdudlarından kənarda təşkil edilən mərc oyunlarından, lotereyalardan, yarışlardan və müsabiqələrdən əldə edilən nağd pul vəsaiti istisna olmaqla) hansı qaydada və neçə faiz dərəcə ilə vergi tutulur?",
+  answer: "B)	pul şəklində əldə olunan uduşdan (mükaftdan) iştirakla bağlı ödənilən pul vəsaiti (pul qoyuluşu) çıxılmaqla qalan məbləğdən ödəmə mənbəyində 10% dərəcə ilə",
+  options: [
+    "A)	tam məbləğdə vergiyə cəlb olunur",
+    "B)	pul şəklində əldə olunan uduşdan (mükaftdan) iştirakla bağlı ödənilən pul vəsaiti (pul qoyuluşu) çıxılmaqla qalan məbləğdən ödəmə mənbəyində 10% dərəcə ilə",
+    "C)	gəlir vergisindən azaddır",
+    "D)	pul şəklində əldə olunan uduşdan (mükaftdan) iştirakla bağlı ödənilən pul vəsaiti (pul qoyuluşu) çıxılmaqla qalan məbləğdən ödəmə mənbəyində 14% dərəcə ilə"
+  ]
+},
+{
+  numb: 6,
+  question: "Вопрос номер 1?",
   answer: "1",
   options: [
     "1",
@@ -44,9 +66,97 @@ let questions = [
     "4"
   ]
 },
-  {
-  numb: 5,
-  question: "Вопрос номер 5?",
+{
+  numb: 6,
+  question: "Вопрос номер 1?",
+  answer: "1",
+  options: [
+    "1",
+    "2",
+    "3",
+    "4"
+  ]
+},
+{
+  numb: 6,
+  question: "Вопрос номер 1?",
+  answer: "1",
+  options: [
+    "1",
+    "2",
+    "3",
+    "4"
+  ]
+},
+{
+  numb: 6,
+  question: "Вопрос номер 1?",
+  answer: "1",
+  options: [
+    "1",
+    "2",
+    "3",
+    "4"
+  ]
+},
+{
+  numb: 6,
+  question: "Вопрос номер 1?",
+  answer: "1",
+  options: [
+    "1",
+    "2",
+    "3",
+    "4"
+  ]
+},
+{
+  numb: 6,
+  question: "Вопрос номер 1?",
+  answer: "1",
+  options: [
+    "1",
+    "2",
+    "3",
+    "4"
+  ]
+},
+{
+  numb: 6,
+  question: "Вопрос номер 1?",
+  answer: "1",
+  options: [
+    "1",
+    "2",
+    "3",
+    "4"
+  ]
+},
+{
+  numb: 6,
+  question: "Вопрос номер 1?",
+  answer: "1",
+  options: [
+    "1",
+    "2",
+    "3",
+    "4"
+  ]
+},
+{
+  numb: 6,
+  question: "Вопрос номер 1?",
+  answer: "1",
+  options: [
+    "1",
+    "2",
+    "3",
+    "4"
+  ]
+},
+{
+  numb: 6,
+  question: "Вопрос номер 1?",
   answer: "1",
   options: [
     "1",
