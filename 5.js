358a53871b395e08b7ed9385c013082be0252218
bo0kproject <143459131+bo0kproject@users.noreply.github.com>
2023-09-11 16:59:41 +0400

    Update 5.js

--- 5.js
+++ 5.js
@@ -2,51 +2,73 @@
 let questions = [
   {
   numb: 1,
-  question: "Вопрос номер 1?",
-  answer: "1",
+  question: "Publik hüquqi şəxslərin Vergi Məcəlləsinin 164.1.48-ci maddəsində göstərilmiş işlərin görülməsi və xidmətlərin göstərilməsindən (həmin maddədə nəzərdə tutulan müddətdə) başqa digər ödənişli işlər görməsindən və xidmətlər göstərməsindən əldə olunan gəlirlərindən və faiz gəlirlərindən mənfəət vergisi necə tutulur?",
+  answer: "D) Ümumi qaydada vergi tutulur",
   options: [
-    "1",
-    "2",
-    "3",
-    "4"
+    "A)	Vergi tutulmur",
+    "B)	10 faiz dərəcəsi ilə vergi tutulur",
+    "C) Xərclər çıxılmadan vergi tutulur",
+    "D) Ümumi qaydada vergi tutulur"
   ]
 },
-  {
+{
   numb: 2,
-  question: "Вопрос номер 2?",
-  answer: "1",
+  question: "İpoteka kreditləşməsi və sahibkarların aldıqları kreditlərə təminat verilməsi sahəsində fəaliyyət göstərən Azərbaycan Respublikasının İpoteka və Kredit Zəmanət Fondu gəlirlərindən mənfəət vergisi tutulurmu?",
+  answer: "B)	Vergi tutulmur",
   options: [
-    "1",
-    "2",
-    "3",
-    "4"
+    "A)	20 faiz dərəcəsi ilə vergi tutulur",
+    "B)	Vergi tutulmur",
+    "C)	Əməliyyat xərcləri çıxıldıqdan sonra vergi tutulur",
+    "D)	Xərclər çıxılmadan vergi tutulur"
   ]
 },
-  {
+{
   numb: 3,
-  question: "Вопрос номер 3?",
-  answer: "1",
+  question: "Azərbaycan Respublikası Dövlət Neft Fondunun gəlirlərindən mənfəət vergisi hansı dərəcə ilə tutulur?",
+  answer: "B)	Vergi tutulmur",
   options: [
-    "1",
-    "2",
-    "3",
-    "4"
+    "A)	20 faiz dərəcəsi ilə vergi tutulur",
+    "B)	Vergi tutulmur",
+    "C)	Əməliyyat xərcləri çıxıldıqdan sonra vergi tutulur",
+    "D)	Xərclər çıxılmadan vergi tutulur"
   ]
 },
-  {
+{
   numb: 4,
-  question: "Вопрос номер 4?",
-  answer: "1",
+  question: "Əmanətlərin Sığortalanması Fondunun gəlirləri mənfəət vergisinə hansı dərəcə ilə cəlb olunur?",
+  answer: "B)	Vergi tutulmur",
   options: [
-    "1",
-    "2",
-    "3",
-    "4"
+    "A) 20 faiz",
+    "B)	Vergi tutulmur",
+    "C) 10 faiz ",
+    "D) 22 faiz "
   ]
 },
-  {
+{
   numb: 5,
-  question: "Вопрос номер 5?",
+  question: "Azərbaycan Respublikasının Mərkəzi Bankının, onun qurumlarının gəlirlərindən mənfəət vergisi tutulurmu?",
+  answer: "B)	Vergi tutulmur",
+  options: [
+    "A)	20 faiz dərəcəsi ilə vergi tutulur",
+    "B)	Vergi tutulmur",
+    "C)	gəlirlərinin 50 faizindən vergi tutulur",
+    "D)	Xərclər çıxılmadan vergi tutulur"
+  ]
+},
+{
+  numb: 6,
+  question: "Nizamnaməyə uyğun olaraq ayrılmış güzəştli kreditlər üzrə müvəkkil kredit təşkilatlan tərəfindən Fonda ödənilən faiz gəlirlərindən mənfəət vergisi tutulurmu?",
+  answer: "D)	Vergi tutulmur",
+  options: [
+    "A)	20 faiz dərəcəsi ilə vergi tutulur",
+    "B)	Xərclər çıxılmadan vergi tutulur",
+    "C)	Gəlirlərinin 50 faizindən vergi tutulur",
+    "D)	Vergi tutulmur"
+  ]
+},
+{
+  numb: 1,
+  question: "Вопрос",
   answer: "1",
   options: [
     "1",
@@ -56,8 +78,162 @@ let questions = [
   ]
 },
 {
-  numb: 6,
-  question: "Вопрос номер 1?",
+  numb: 1,
+  question: "Вопрос",
+  answer: "1",
+  options: [
+    "1",
+    "2",
+    "3",
+    "4"
+  ]
+},
+{
+  numb: 1,
+  question: "Вопрос",
+  answer: "1",
+  options: [
+    "1",
+    "2",
+    "3",
+    "4"
+  ]
+},
+{
+  numb: 1,
+  question: "Вопрос",
+  answer: "1",
+  options: [
+    "1",
+    "2",
+    "3",
+    "4"
+  ]
+},
+{
+  numb: 1,
+  question: "Вопрос",
+  answer: "1",
+  options: [
+    "1",
+    "2",
+    "3",
+    "4"
+  ]
+},
+{
+  numb: 1,
+  question: "Вопрос",
+  answer: "1",
+  options: [
+    "1",
+    "2",
+    "3",
+    "4"
+  ]
+},
+{
+  numb: 1,
+  question: "Вопрос",
+  answer: "1",
+  options: [
+    "1",
+    "2",
+    "3",
+    "4"
+  ]
+},
+{
+  numb: 1,
+  question: "Вопрос",
+  answer: "1",
+  options: [
+    "1",
+    "2",
+    "3",
+    "4"
+  ]
+},
+{
+  numb: 1,
+  question: "Вопрос",
+  answer: "1",
+  options: [
+    "1",
+    "2",
+    "3",
+    "4"
+  ]
+},
+{
+  numb: 1,
+  question: "Вопрос",
+  answer: "1",
+  options: [
+    "1",
+    "2",
+    "3",
+    "4"
+  ]
+},
+{
+  numb: 1,
+  question: "Вопрос",
+  answer: "1",
+  options: [
+    "1",
+    "2",
+    "3",
+    "4"
+  ]
+},
+{
+  numb: 1,
+  question: "Вопрос",
+  answer: "1",
+  options: [
+    "1",
+    "2",
+    "3",
+    "4"
+  ]
+},
+{
+  numb: 1,
+  question: "Вопрос",
+  answer: "1",
+  options: [
+    "1",
+    "2",
+    "3",
+    "4"
+  ]
+},
+{
+  numb: 1,
+  question: "Вопрос",
+  answer: "1",
+  options: [
+    "1",
+    "2",
+    "3",
+    "4"
+  ]
+},
+{
+  numb: 1,
+  question: "Вопрос",
+  answer: "1",
+  options: [
+    "1",
+    "2",
+    "3",
+    "4"
+  ]
+},
+{
+  numb: 1,
+  question: "Вопрос",
   answer: "1",
   options: [
     "1",
