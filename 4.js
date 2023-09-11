eb6e5d95aae41b866572ced5b2b0cbb5f310d1d1
bo0kproject <143459131+bo0kproject@users.noreply.github.com>
2023-09-11 15:58:10 +0400

    Update 4.js

--- 4.js
+++ 4.js
@@ -57,57 +57,57 @@ let questions = [
 },
 {
   numb: 6,
-  question: "Вопро",
-  answer: "1",
+  question: "Qeyri-rezidentin daimi nümayəndəliyi ilə bağlı olmayan ümumi gəliri vergiyə cəlb olunurmu?",
+  answer: "D)	Vergi Məcəlləsinin 125-ci maddəsində nəzərdə tutulan qaydada ödəmə mənbəyində xərclər çıxılmadan vergiyə cəlb olunur",
   options: [
-    "1",
-    "2",
-    "3",
-    "4"
+    "A)	Vergiyə cəlb edilmir",
+    "B)	Xərclər çıxıldıqdan sonra vergiyə cəlb olunur",
+    "C)	Ödəmə mənbəyində 10 faiz dərəcə ilə vergiyə cəlb olunur",
+    "D)	Vergi Məcəlləsinin 125-ci maddəsində nəzərdə tutulan qaydada ödəmə mənbəyində xərclər çıxılmadan vergiyə cəlb olunur"
   ]
 },
   {
-  numb: 6,
-  question: "Вопро",
-  answer: "1",
+  numb: 7,
+  question: "Qeyri-rezidentin daimi nümayəndəliklə bağlı olmayan dividenddən gəliri mənfəət (gəlir) vergisinə necə cəlb olunur?",
+  answer: "A)	Ödəmə mənbəyində 10 faiz dərəcə ilə vergiyə cəlb olunur",
   options: [
-    "1",
-    "2",
-    "3",
-    "4"
+    "A)	Odəmə mənbəyində 10 faiz dərəcə ilə vergiyə cəlb olunur",
+    "B)	Xərclər çıxıldıqdan sonra vergiyə cəlb olunur",
+    "C)	Vergiyə cəlb edilmir",
+    "D)	Ödəmə mənbəyində 14 faiz dərəcə ilə vergiyə cəlb olunur"
   ]
 },
   {
-  numb: 6,
-  question: "Вопро",
-  answer: "1",
+  numb: 8,
+  question: "Azərbaycan Respublikasındakı daimi nümayəndəliklə bağlı olmayan əmlak təqdim edilməsindən gəlir əldə edən qeyri-rezident müəssisəsinin mənfəətindən vergi necə tutulur?",
+  answer: "A)	Təqvim ili ərzində Azərbaycan mənbəyindən əldə etdiyi əmlak təqdim edilməsindən ümumi gəlirindən Vergi Məcəlləsində göstərilən və həmin dövrdə bu cür gəlirə aid olan xərclər çıxıldıqdan sonra vergi tutulur",
   options: [
-    "1",
-    "2",
-    "3",
-    "4"
+    "A)	Təqvim ili ərzində Azərbaycan mənbəyindən əldə etdiyi əmlak təqdim edilməsindən ümumi gəlirindən Vergi Məcəlləsində göstərilən və həmin dövrdə bu cür gəlirə aid olan xərclər çıxıldıqdan sonra vergi tutulur",
+    "B)	Təqvim ili ərzində Azərbaycan mənbəyindən əldə etdiyi bu növ ümumi gəlirindən həmin dövrdə bu cür gəlirə aid olan xərclər çıxılmadan vergi tutulur",
+    "C)	Vergi tutulmur",
+    "D)	Təqvim ili ərzində Azərbaycan mənbəyindən əldə etdiyi bu növ ümumi gəlirindən həmin dövrdə bütün xərcləri çıxıldıqdan sonra vergi tutulur"
   ]
 },
   {
-  numb: 6,
-  question: "Вопро",
-  answer: "1",
+  numb: 9,
+  question: "Əsas vəsaitlərin yenidən qiymətləndirilməsindən yaranan artım (yenidən qiymətlən-dirilmə nəticəsində yaranan müsbət fərq) mənfəət vergisinə hansı qaydada cəlb olunur?",
+  answer: "B)	Cəlb olunmur ",
   options: [
-    "1",
-    "2",
-    "3",
-    "4"
+    "A)	Ümumi qaydada ",
+    "B)	Cəlb olunmur ",
+    "C) 10%- lə cəlb olunur",
+    "D) xüsusi hallarda cəlb olunur"
   ]
 },
   {
-  numb: 6,
-  question: "Вопро",
-  answer: "1",
+  numb: 10,
+  question: "Aşağıda göstərilənlərdən hansı mənfəət vergisinin vergitutma obyekti deyildir?",
+  answer: "B)	Əsas vəsaitlərin (vəsaitin) yenidən qiymətləndirilməsindən yaranan artım (yenidən qiymət¬ləndirilmə nəticəsində yaranan müsbət fərq)",
   options: [
-    "1",
-    "2",
-    "3",
-    "4"
+    "A)	Əsas vəsaitlərin təqdim edilməsindən alınan və Vergi Məcəlləsinin 114.7-ci maddəsinə uyğun olaraq gəlirə daxil edilən məbləğlər",
+    "B)	Əsas vəsaitlərin (vəsaitin) yenidən qiymətləndirilməsindən yaranan artım (yenidən qiymət¬ləndirilmə nəticəsində yaranan müsbət fərq)",
+    "C) Əlillərin və ya sağlamlıq imkanları məhdud uşaqların əlilliyi olan şəxslərin qeyri-hökumət təşkilatlarına məxsus olan işçilərinin ümumi sayının azı 50 faizi əlillərdən və ya sağlamlıq imkanları məhdud 18 yaşınadək şəxslərdən ibarət olan istehsal müəssisələrinin gəliri",
+    "D)	Kommersiya təşkilatlarının aldıqları əvəzsiz köçürmələr"
   ]
 },
   {
