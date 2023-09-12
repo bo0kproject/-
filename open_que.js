5ef08192996128a9761b76f949fdb29d78238abf
bo0kproject <143459131+bo0kproject@users.noreply.github.com>
2023-09-12 17:41:28 +0400

    Create open_que.js

---
+++ open_que.js
@@ -0,0 +1,22 @@
+document.querySelector("#que_1").onclick = function(){
+  alert("Вы нажали на кнопку");
+}
+document.querySelector("#que_2").onclick = function(){
+  alert("Вы нажали на кнопку");
+}
+document.querySelector("#que_3").onclick = function(){
+  alert("Вы нажали на кнопку");
+}
+document.querySelector("#que_4").onclick = function(){
+  alert("Вы нажали на кнопку");
+}
+document.querySelector("#que_5").onclick = function(){
+  alert("Вы нажали на кнопку");
+}
+
+document.querySelector("#que_6").onclick = function(){
+  alert("Вы нажали на кнопку");
+}
+document.querySelector("#que_7").onclick = function(){
+  alert("Вы нажали на кнопку");
+}
