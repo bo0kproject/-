c8d83b798b5872214463b847d64ff2c330b9d445
bo0kproject <143459131+bo0kproject@users.noreply.github.com>
2023-09-12 18:21:27 +0400

    Update open_1.js

--- open_1.js
+++ open_1.js
@@ -1,8 +1,33 @@
 // создание вопросов и ответов
 let questions = [
   {
-  numb: 1,2,3,4,5,
+  numb: 1,
   question: "Вопрос номер 1?",
+  answer: "Duz",
+  options: [
+    "Duz",
+    "Sehv"
+  ]
+},
+  {
+  numb: 2,
+  question: "Вопрос номер 2?",
+  answer: "Duz",
+  options: [
+    "Duz",
+    "Sehv"
+  ]
+},{
+  numb: 3,
+  question: "Вопрос номер 3?",
+  answer: "Duz",
+  options: [
+    "Duz",
+    "Sehv"
+  ]
+},{
+  numb: 4,
+  question: "Вопрос номер 4?",
   answer: "Duz",
   options: [
     "Duz",
