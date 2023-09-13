fc2de34410e15853fe4a85f03b117516af3ea262
bo0kproject <143459131+bo0kproject@users.noreply.github.com>
2023-09-13 14:22:19 +0400

    Update 6.js

--- 6.js
+++ 6.js
@@ -2,35 +2,35 @@
 let questions = [
   {
   numb: 1,
-  question: "Вопрос номер 1?",
-  answer: "1",
+  question: "Hansı xərclərin gəlirdən çıxılmasına yol verilir?",
+  answer: "C)	Gəlirdən çıxılmayan xərclərdən başqa, gəlirin əldə edilməsi ilə bağlı olan bütün xərclər, həmçinin qanunla nəzərdə tutulmuş icbari ödənişlərin",
   options: [
-    "1",
-    "2",
-    "3",
-    "4"
+    "A)	Gəlirdən çıxılmayan xərclərdən başqa, gəlirin əldə edilməsi ilə bağlı olan xərclərin",
+    "B)	Gəlirin əldə edilməsi ilə bağlı olan bütün xərclərin",
+    "C)	Gəlirdən çıxılmayan xərclərdən başqa, gəlirin əldə edilməsi ilə bağlı olan bütün xərclər, həmçinin qanunla nəzərdə tutulmuş icbari ödənişlərin",
+    "D)	Bütün xərclərin, həmçinin qanunla nəzərdə tutulmuş icbari ödənişlərin, faizlərin, maliyyə sanksiyalarının və inzibati cərimələrin"
   ]
 },
   {
   numb: 2,
-  question: "Вопрос номер 2?",
-  answer: "1",
+  question: "Aşağıdakı hallarda bölüşdürülməsi mümkün olmayan xərclərin gəlirdən çıxılan məbləği necə müəyyən edilir?",
+  answer: "A) Vergi ödəyicisi tərəfindən vergiyə cəlb olunan və vergidən azad olunan, yaxud mənfəət (gəlir) vergisinə cəlb edilməyən, habelə qeyri-sahibkarhq fəaliyyəti ilə bağlı əməliyyatlar üzrə çəkilən və həmin əməliyyatlar üzrə gəlirlərin və xərclərin uçotu ayrılıqda aparılan, lakin bölüşdürülməsi mümkün olmayan xərclərin gəlirdən çıxılan məbləği vergiyə cəlb edilən gəlirlərin vergi ödəyicisinin ümumi gəlirlərində xüsusi çəkisi əsasında müəyyən edilir",
   options: [
-    "1",
-    "2",
-    "3",
-    "4"
+    "A) Vergi ödəyicisi tərəfindən vergiyə cəlb olunan və vergidən azad olunan, yaxud mənfəət (gəlir) vergisinə cəlb edilməyən, habelə qeyri-sahibkarhq fəaliyyəti ilə bağlı əməliyyatlar üzrə çəkilən və həmin əməliyyatlar üzrə gəlirlərin və xərclərin uçotu ayrılıqda aparılan, lakin bölüşdürülməsi mümkün olmayan xərclərin gəlirdən çıxılan məbləği vergiyə cəlb edilən gəlirlərin vergi ödəyicisinin ümumi gəlirlərində xüsusi çəkisi əsasında müəyyən edilir",
+    "B)	Vergi ödəyicisi tərəfindən vergiyə cəlb olunan və vergidən azad olunan, yaxud mənfəət (gəlir) vergisinə cəlb edilməyən, habelə qeyri-sahibkarlıq fəaliyyəti ilə bağlı əməliyyatlar üzrə çəkilən və həmin əməliyyatlar üzrə gəlirlərin və xərclərin uçotu birgə aparılan, lakin bölüşdürülməsi mümkün olmayan xərclərin gəlirdən çıxılan məbləği vergiyə cəlb edilən gəlirlərin vergi ödəyicisinin ümumi gəlirlərində xüsusi çəkisi əsasında müəyyən edilir",
+    "C)	Vergi ödəyicisi tərəfindən vergiyə cəlb olunan və vergidən azad olunan, yaxud mənfəət (gəlir) vergisinə cəlb edilməyən, habelə qeyri-sahibkarlıq fəaliyyəti ilə bağlı əməliyyatlar üzrə çəkilən və həmin əməliyyatlar üzrə gəlirlərin və xərclərin uçotu ayrılıqda aparılan, lakin bölüşdürülməsi mümkün olmayan xərclərin gəlirdən çıxılan məbləği vergiyə cəlb edilən gəlirlərin vergi ödəyicisinin digər gəlirlərində xüsusi çəkisi əsasında müəyyən edilir",
+    "D)	Vergiyə cəlb edilən əməliyyatlarla yanaşı, vergidən azad olunan əməliyyatlar da aparıldıqda həmin əməliyyatlar üzrə gəlirlərin və xərclərin uçotu ayrılıqda aparılmalı və xərclərin gəlirdən çıxılan məbləği vergidən azad edilən gəlirlərin vergi ödəyicisinin ümumi gəlirlərində xüsusi çəkisi əsasında müəyyən olunmalıdır"
   ]
 },
   {
   numb: 3,
-  question: "Вопрос номер 3?",
-  answer: "1",
+  question: "Bina tikintisi fəaliyyəti ilə məşğul olan şəxslər tərəfindən dövlətə ayrılan hissə təqdim edildikdə, aktivlərə çəkilmiş məsrəflər gəlirdən çıxılırmı",
+  answer: "B)	Çıxıhr",
   options: [
-    "1",
-    "2",
-    "3",
-    "4"
+    "A)	Çıxılmır ",
+    "B)	Çıxıhr",
+    "C) Nazirlər Kabinetinin qərarı əsasında çıxılır",
+    "D) 50%-i çıxılır"
   ]
 },
   {
@@ -58,6 +58,303 @@ let questions = [
 {
   numb: 6,
   question: "Вопрос номер 1?",
+  answer: "1",
+  options: [
+    "1",
+    "2",
+    "3",
+    "4"
+  ]
+},
+  {
+  numb: 1,
+  question: "Вопрос",
+  answer: "1",
+  options: [
+    "1",
+    "2",
+    "3",
+    "4"
+  ]
+},
+  {
+  numb: 1,
+  question: "Вопрос",
+  answer: "1",
+  options: [
+    "1",
+    "2",
+    "3",
+    "4"
+  ]
+},
+  {
+  numb: 1,
+  question: "Вопрос",
+  answer: "1",
+  options: [
+    "1",
+    "2",
+    "3",
+    "4"
+  ]
+},
+  {
+  numb: 1,
+  question: "Вопрос",
+  answer: "1",
+  options: [
+    "1",
+    "2",
+    "3",
+    "4"
+  ]
+},
+  {
+  numb: 1,
+  question: "Вопрос",
+  answer: "1",
+  options: [
+    "1",
+    "2",
+    "3",
+    "4"
+  ]
+},
+  {
+  numb: 1,
+  question: "Вопрос",
+  answer: "1",
+  options: [
+    "1",
+    "2",
+    "3",
+    "4"
+  ]
+},
+  {
+  numb: 1,
+  question: "Вопрос",
+  answer: "1",
+  options: [
+    "1",
+    "2",
+    "3",
+    "4"
+  ]
+},
+  {
+  numb: 1,
+  question: "Вопрос",
+  answer: "1",
+  options: [
+    "1",
+    "2",
+    "3",
+    "4"
+  ]
+},
+  {
+  numb: 1,
+  question: "Вопрос",
+  answer: "1",
+  options: [
+    "1",
+    "2",
+    "3",
+    "4"
+  ]
+},
+  {
+  numb: 1,
+  question: "Вопрос",
+  answer: "1",
+  options: [
+    "1",
+    "2",
+    "3",
+    "4"
+  ]
+},
+  {
+  numb: 1,
+  question: "Вопрос",
+  answer: "1",
+  options: [
+    "1",
+    "2",
+    "3",
+    "4"
+  ]
+},
+  {
+  numb: 1,
+  question: "Вопрос",
+  answer: "1",
+  options: [
+    "1",
+    "2",
+    "3",
+    "4"
+  ]
+},
+  {
+  numb: 1,
+  question: "Вопрос",
+  answer: "1",
+  options: [
+    "1",
+    "2",
+    "3",
+    "4"
+  ]
+},
+  {
+  numb: 1,
+  question: "Вопрос",
+  answer: "1",
+  options: [
+    "1",
+    "2",
+    "3",
+    "4"
+  ]
+},
+  {
+  numb: 1,
+  question: "Вопрос",
+  answer: "1",
+  options: [
+    "1",
+    "2",
+    "3",
+    "4"
+  ]
+},
+  {
+  numb: 1,
+  question: "Вопрос",
+  answer: "1",
+  options: [
+    "1",
+    "2",
+    "3",
+    "4"
+  ]
+},
+  {
+  numb: 1,
+  question: "Вопрос",
+  answer: "1",
+  options: [
+    "1",
+    "2",
+    "3",
+    "4"
+  ]
+},
+  {
+  numb: 1,
+  question: "Вопрос",
+  answer: "1",
+  options: [
+    "1",
+    "2",
+    "3",
+    "4"
+  ]
+},
+  {
+  numb: 1,
+  question: "Вопрос",
+  answer: "1",
+  options: [
+    "1",
+    "2",
+    "3",
+    "4"
+  ]
+},
+  {
+  numb: 1,
+  question: "Вопрос",
+  answer: "1",
+  options: [
+    "1",
+    "2",
+    "3",
+    "4"
+  ]
+},
+  {
+  numb: 1,
+  question: "Вопрос",
+  answer: "1",
+  options: [
+    "1",
+    "2",
+    "3",
+    "4"
+  ]
+},
+  {
+  numb: 1,
+  question: "Вопрос",
+  answer: "1",
+  options: [
+    "1",
+    "2",
+    "3",
+    "4"
+  ]
+},
+  {
+  numb: 1,
+  question: "Вопрос",
+  answer: "1",
+  options: [
+    "1",
+    "2",
+    "3",
+    "4"
+  ]
+},
+  {
+  numb: 1,
+  question: "Вопрос",
+  answer: "1",
+  options: [
+    "1",
+    "2",
+    "3",
+    "4"
+  ]
+},
+  {
+  numb: 1,
+  question: "Вопрос",
+  answer: "1",
+  options: [
+    "1",
+    "2",
+    "3",
+    "4"
+  ]
+},
+  {
+  numb: 1,
+  question: "Вопрос",
+  answer: "1",
+  options: [
+    "1",
+    "2",
+    "3",
+    "4"
+  ]
+},
+  {
+  numb: 1,
+  question: "Вопрос",
   answer: "1",
   options: [
     "1",
