95fa0e8156053de4154b786ca75193cbec183da3
bo0kproject <143459131+bo0kproject@users.noreply.github.com>
2023-09-14 18:45:02 +0400

    Update 10.js

--- 10.js
+++ 10.js
@@ -2,24 +2,24 @@
 let questions = [
   {
   numb: 1,
-  question: "Вопрос номер 1?",
-  answer: "1",
+  question: "173.	Maliyyə lizinqi müqaviləsi üzrə lizinq alan qismində kimlər iştirak edə bilər?",
+  answer: "D)	Yalnız hüquqi şəxslər və fərdi sahibkarlar",
   options: [
-    "1",
-    "2",
-    "3",
-    "4"
+    "A)	İstənilən şəxs, qeyri-rezidentlər və onların daimi nümayəndəlikləri",
+    "B)	Yalnız fərdi sahibkarlar",
+    "C)	Yalnız hüquqi şəxslər",
+    "D)	Yalnız hüquqi şəxslər və fərdi sahibkarlar"
   ]
 },
   {
   numb: 2,
-  question: "Вопрос номер 2?",
-  answer: "1",
+  question: "Aşağıdakı hallardan hansılar olduqda əmlak icarəsi maliyyə lizinqi sayılır? 1.	icarə müddəti icarəyə götürülən əmlakın istismar müddətinin 75 faizind artıq olduğu halda 2.	icarə müddəti qurtardıqdan sonra əmlakın qalıq dəyəri icarənin əvvəlinə onun bazar qiymətinin 20 faizindən az olduğu halda 3.	icarəyə götürülən əmlak icarəçi üçün sifarişlə hazırlandığı və icarə müddəti qurtardıqdan sonra icarəçidən başqa heç kəs tərəfindən istifadə edilə bilmədiyi halda 4.	lizinq müqaviləsi ilə icarə müddəti qurtardıqdan sonra əmlakın mülkiyyətə verilməsi nəzərdə tutulmadığı halda 5. icarə müddəti icarəyə götürülən əmlakın istismar müddətinin 75 faizindən az olduğu halda",
+  answer: "A) 1,2,3",
   options: [
-    "1",
-    "2",
-    "3",
-    "4"
+    "A) 1,2,3",
+    "B) 1,3,4",
+    "C) 2,4,5",
+    "D) 3,4,5"
   ]
 },
   {
