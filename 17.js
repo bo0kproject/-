3d5104536c588017ce0993ca4f1ff388f42339f5
bo0kproject <143459131+bo0kproject@users.noreply.github.com>
2023-09-16 14:23:34 +0400

    Update 17.js

--- 17.js
+++ 17.js
@@ -375,69 +375,69 @@ let questions = [
   ]
 },
 {
-  numb: 6,
-  question: "Вопрос",
-  answer: "1",
+  numb: 35,
+  question: "Riskli vergi ödəyiciləri tərəfindən artıq ödəmənin qaytarılması ilə bağlı müraciət edildikdə nə edilməlidir?",
+  answer: "C)	Vergi Məcəlləsinin 179-cu maddəsi ilə müəyyən edilən müddətlər onların fəaliyyəti üzrə kameral və ya səyyar vergi yoxlamaları və operativ vergi nəzarəti tədbirləri tam başa çatdıqdan sonra tətbiq edilir",
   options: [
-    "1",
-    "2",
-    "3",
-    "4"
+    "A)	Riskli vergi ödəyiciləri tərəfindən artıq ödənilmiş məbləğər geri qaytarılmır",
+    "B)	Onların fəaliyyəti üzrə kameral vergi yoxlamaları tam başa çatdıqdan sonra Vergi Məcəlləsinin 179-cu maddəsi ilə müəyyən edilən müddətlər tətbiq edilir",
+    "C)	Vergi Məcəlləsinin 179-cu maddəsi ilə müəyyən edilən müddətlər onların fəaliyyəti üzrə kameral və ya səyyar vergi yoxlamaları və operativ vergi nəzarəti tədbirləri tam başa çatdıqdan sonra tətbiq edilir",
+    "D)	Vergi Məcəlləsinin 179-cu maddəsi ilə müəyyən edilən müddətlər tətbiq edilir"
   ]
 },
 {
-  numb: 6,
-  question: "Вопрос",
-  answer: "1",
+  numb: 36,
+  question: "ƏDV-nin düzgün hesablanması və vaxtında ödənilməsinə kim məsuliyyət daşıyır?",
+  answer: "A)	vergi ödəyiciləri və onun vəzifəli şəxsləri",
   options: [
-    "1",
-    "2",
-    "3",
-    "4"
+    "A)	vergi ödəyiciləri və onun vəzifəli şəxsləri",
+    "B)	Gömrük orqanları və onların vəzifəli şəxsləri",
+    "C)	Gömrük orqanları və onların vəzifəli şəxsləri",
+    "D)	Maliyyə orqanları və onların vəzifəli şəxsləri"
   ]
 },
 {
-  numb: 6,
-  question: "Вопрос",
-  answer: "1",
+  numb: 37,
+  question: "ƏDV bəyannaməsinin vaxtında təqdim edilməsi üçün kim məsuliyyət daşıyır?",
+  answer: "D)	Vergi ödəyicisi və onun vəzifəli şəxsləri",
   options: [
-    "1",
-    "2",
-    "3",
-    "4"
+    "A)	Vergi orqanları və onların vəzifəli şəxsləri",
+    "B)	Gömrük orqanları və onların vəzifəli şəxsləri",
+    "C)	Maliyyə orqanları və onların vəzifəli şəxsləri",
+    "D)	Vergi ödəyicisi və onun vəzifəli şəxsləri"
   ]
 },
 {
-  numb: 6,
-  question: "Вопрос",
-  answer: "1",
+  numb: 38,
+  question: "Malların idxalı zamanı ƏDV-nin tutulmasına kim məsuliyyət daşıyır?",
+  answer: "B)	Gömrük orqanları və onların vəzifəli şəxsləri",
   options: [
-    "1",
-    "2",
-    "3",
-    "4"
+    "A)	Vergi orqanları və onların vəzifəli şəxsləri",
+    "B)	Gömrük orqanları və onların vəzifəli şəxsləri",
+    "C)	Maliyyə orqanları və onların vəzifəli şəxsləri",
+    "D)	Vergi ödəyicisi və onun vəzifəli şəxsləri"
   ]
 },
 {
-  numb: 6,
-  question: "Вопрос",
-  answer: "1",
+  numb: 39,
+  question: "Vergitutmanın idarə edilməsi hansı orqan tərəfindən həyata keçirilir?",
+  answer: "A)	Vergi Məcəlləsinə və Azərbaycan Respublikasının gömrük qanunvericiliyinə uyğun olaraq vergi orqanları və gömrük orqanları tərəfindən öz səlahiyyətləri daxilində həyata keçirilir.",
   options: [
-    "1",
-    "2",
-    "3",
-    "4"
+    "A)	Vergi Məcəlləsinə və Azərbaycan Respublikasının gömrük qanunvericiliyinə uyğun olaraq vergi orqanları və gömrük orqanları tərəfindən öz səlahiyyətləri daxilində həyata keçirilir.",
+    "B)	Vergi Məcəlləsinə uyğun olaraq vergi orqanları və onların vəzifəli şəxsləri tərəfindən öz səlahiyyətləri daxilində həyata keçirilir.",
+    "C)	Vergi Məcəlləsinə uyğun olaraq vergi və maliyyə orqanları və onların vəzifəli şəxsləri tərəfindən öz səlahiyyətləri daxilində həyata keçirilir.",
+    "D)	Vergi ödəyicisi və onun vəzifəli şəxsləri tərəfindən həyata keçirilir."
   ]
 },
 {
-  numb: 6,
-  question: "Вопрос",
-  answer: "1",
+  numb: 40,
+  question: "Azərbaycan Respublikasının tərəfdar çıxdığı beynəlxalq müqaviləyə uyğun olaraq beynəlxalq təşkilatın, yaxud xarici hüquqi və ya fiziki şəxsin əvəzsiz köçürmələri ilə ödənilmiş malları alan və yaxud işlərin və ya xidmətlərin nəticələrini həmin əsaslarla əldə edən şəxs vergi orqanına elektron qaimə-fakturaları verərkən, həmin mallara, işlərə və ya xidmətlərə görə ödənilmiş ƏDV-ni vergi orqanına ərizə verdiyi vaxtdan hansı müddət ərzində geri almaq hüququna malikdir?",
+  answer: "D) 45 gün",
   options: [
-    "1",
-    "2",
-    "3",
-    "4"
+    "A) 10 gün",
+    "B) 20 gün",
+    "C) 30 gün",
+    "D) 45 gün"
   ]
 },
 ];
