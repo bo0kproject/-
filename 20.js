4db452a8f62f0defd257d6b9e92be012d4deaa4c
bo0kproject <143459131+bo0kproject@users.noreply.github.com>
2023-09-18 13:26:38 +0400

    Update 20.js

--- 20.js
+++ 20.js
@@ -57,13 +57,398 @@ let questions = [
 },
 {
   numb: 6,
-  question: "Вопрос номер 1?",
+  question: "?",
   answer: "1",
   options: [
     "1",
     "2",
     "3",
     "4"
+  ]
+},
+  {
+  numb: 6,
+  question: "?",
+  answer: "1",
+  options: [
+    "1",
+    "2",
+    "3",
+    "4"
+  ]
+},
+{
+  numb: 6,
+  question: "?",
+  answer: "1",
+  options: [
+    "1",
+    "2",
+    "3",
+    "4"
+  ]
+},
+{
+  numb: 6,
+  question: "?",
+  answer: "1",
+  options: [
+    "1",
+    "2",
+    "3",
+    "4"
+  ]
+},
+{
+  numb: 6,
+  question: "?",
+  answer: "1",
+  options: [
+    "1",
+    "2",
+    "3",
+    "4"
+  ]
+},
+{
+  numb: 6,
+  question: "?",
+  answer: "1",
+  options: [
+    "1",
+    "2",
+    "3",
+    "4"
+  ]
+},
+{
+  numb: 6,
+  question: "?",
+  answer: "1",
+  options: [
+    "1",
+    "2",
+    "3",
+    "4"
+  ]
+},
+{
+  numb: 6,
+  question: "?",
+  answer: "1",
+  options: [
+    "1",
+    "2",
+    "3",
+    "4"
+  ]
+},
+{
+  numb: 6,
+  question: "?",
+  answer: "1",
+  options: [
+    "1",
+    "2",
+    "3",
+    "4"
+  ]
+},
+{
+  numb: 6,
+  question: "?",
+  answer: "1",
+  options: [
+    "1",
+    "2",
+    "3",
+    "4"
+  ]
+},
+{
+  numb: 6,
+  question: "?",
+  answer: "1",
+  options: [
+    "1",
+    "2",
+    "3",
+    "4"
+  ]
+},
+{
+  numb: 6,
+  question: "?",
+  answer: "1",
+  options: [
+    "1",
+    "2",
+    "3",
+    "4"
+  ]
+},
+{
+  numb: 6,
+  question: "?",
+  answer: "1",
+  options: [
+    "1",
+    "2",
+    "3",
+    "4"
+  ]
+},
+{
+  numb: 6,
+  question: "?",
+  answer: "1",
+  options: [
+    "1",
+    "2",
+    "3",
+    "4"
+  ]
+},
+{
+  numb: 6,
+  question: "?",
+  answer: "1",
+  options: [
+    "1",
+    "2",
+    "3",
+    "4"
+  ]
+},
+{
+  numb: 6,
+  question: "?",
+  answer: "1",
+  options: [
+    "1",
+    "2",
+    "3",
+    "4"
+  ]
+},
+{
+  numb: 6,
+  question: "?",
+  answer: "1",
+  options: [
+    "1",
+    "2",
+    "3",
+    "4"
+  ]
+},
+{
+  numb: 6,
+  question: "?",
+  answer: "1",
+  options: [
+    "1",
+    "2",
+    "3",
+    "4"
+  ]
+},
+{
+  numb: 6,
+  question: "?",
+  answer: "1",
+  options: [
+    "1",
+    "2",
+    "3",
+    "4"
+  ]
+},
+{
+  numb: 6,
+  question: "?",
+  answer: "1",
+  options: [
+    "1",
+    "2",
+    "3",
+    "4"
+  ]
+},
+{
+  numb: 6,
+  question: "?",
+  answer: "1",
+  options: [
+    "1",
+    "2",
+    "3",
+    "4"
+  ]
+},
+{
+  numb: 6,
+  question: "?",
+  answer: "1",
+  options: [
+    "1",
+    "2",
+    "3",
+    "4"
+  ]
+},
+{
+  numb: 6,
+  question: "?",
+  answer: "1",
+  options: [
+    "1",
+    "2",
+    "3",
+    "4"
+  ]
+},
+{
+  numb: 6,
+  question: "?",
+  answer: "1",
+  options: [
+    "1",
+    "2",
+    "3",
+    "4"
+  ]
+},
+{
+  numb: 6,
+  question: "?",
+  answer: "1",
+  options: [
+    "1",
+    "2",
+    "3",
+    "4"
+  ]
+},
+{
+  numb: 6,
+  question: "?",
+  answer: "1",
+  options: [
+    "1",
+    "2",
+    "3",
+    "4"
+  ]
+},
+{
+  numb: 6,
+  question: "?",
+  answer: "1",
+  options: [
+    "1",
+    "2",
+    "3",
+    "4"
+  ]
+},
+{
+  numb: 33,
+  question: "Aksizli mallar üzərində vergi nəzarəti zamanı aşağıdakı hansı hallar olduqda nəzarət postlarının işçiləri tərəfindən istehsalçının nümayəndəsi ilə birlikdə plomblanma həyata keçirilə bilər? 1)	lisenziyanın qüvvədə olma müddəti dayandırıldıqda və ya ləğv edildikdə; 2)	hazır məhsulun markalanması üçün aksiz markaları olmadıqda; 3)	başa çatmamış istehsalın markalanması üçün aksiz markaları olmadıqda; 4)	istehsal üçün xammal, yardımçı və qablaşdırıcı materiallar olmadıqda; 5)	avadanlıqlarda müvəqqəti təmir aparıldığı zaman.",
+  answer: "B) 1;2;4",
+  options: [
+    "A) 1;2;3",
+    "B) 1;2;4",
+    "C) 3;4;5",
+    "D)l;3;4"
+  ]
+},
+{
+  numb: 34,
+  question: "Aksizli mallar üzərində vergi nəzarətinin həyata keçirilməsi prosesində avadanlığın dayandırılması zamanı istehsal güclərinin qapanması istehsal üzrə texnoloji xətlərin və avadanlığın elə yerlərdə plomblanması vasitəsilə həyata keçirilməlidir ki,	",
+  answer: "B)	Onların istifadə edilməsi mümkün olmasın",
+  options: [
+    "A)	Onların istifadəsi qismən məhdudlaşdırılsın",
+    "B)	Onların istifadə edilməsi mümkün olmasın",
+    "C) Onların istifadəsi qismən mümkün olsun",
+    "D) Yalnız istehsalçının göstərişi ilə istifadə edilə bilsin"
+  ]
+},
+{
+  numb: 35,
+  question: "Aksizli mallar üzərində vergi nəzarəti həyata keçirilərkən istehsal həcmi üzrə nəzarət-ölçü cihazlarına,xammal və hazır məhsul anbarlarına, istehsal sahələrindəki texno¬loji avadanlıqlara plombların vurulması və çıxarılması kim tərəfindən rəsmi-ləşdirilir?",
+  answer: "C)	Nəzarət postunun işçiləri və istehsalçının nümayəndəsinin iştirakı ilə",
+  options: [
+    "A)	Yalnız nəzarət postunun işçiləri tərəfindən",
+    "B)	Yalnız istehsalçının nümayəndələri tərəfindən",
+    "C)	Nəzarət postunun işçiləri və istehsalçının nümayəndəsinin iştirakı ilə",
+    "D)	Heç bir halda rəsmiləşdirilmir"
+  ]
+},
+{
+  numb: 36,
+  question: "Aksizli mallar üzərində vergi nəzarəti həyata keçirilərkən istehsal həcmi üzrə nə-zarət-ölçü cihazlarına, xammal, material və hazır məhsul anbarlarına, istehsal sahələ-rindəki texnoloji avadanlıqlara plombların vurulması və çıxarılması hansı sənədlə rəsmi-ləşdirilir?",
+  answer: "B) Aktla",
+  options: [
+    "A) Protokolla",
+    "B) Aktla",
+    "C) Qərarla",
+    "D) Xüsusi bir sənəd tərtib edilmir"
+  ]
+},
+{
+  numb: 37,
+  question: "Aksizli mallar üzərində vergi nəzarəti zamanı nəzarət-ölçü cihazları, anbarlar və texnoloji avadanlıqlar plomblandıqda hansı halda istehsalçının məsul şəxsləri tərəfindən vergi orqanına məlumat verilməklə və hansı sənəd tərtib edilməklə plomblar açıla bilər?",
+  answer: "A) Nəzarət postlarının işçiləri işə başladığı vaxtda nəzarət postunda olmadıqda müvafiq akt tərtib edilməklə",
+  options: [
+    "A)	Nəzarət postlarının işçiləri işə başladığı vaxtda nəzarət postunda olmadıqda müvafiq akt tərtib edilməklə",
+    "B)	Nəzarət postlarının işçiləri işə başladığı vaxtda nəzarət postunda olub-olmamasından asılı olmayaraq istənilən vaxtda və heç bir sənəd tərtib etmədən",
+    "C)	Heç bir halda açılmasına yol verilmir",
+    "D)	Nəzarət postlarının işçiləri işə başladığı vaxtda və istehsalçıları xüsusi qərarı olduğu halda "
+  ]
+},
+{
+  numb: 38,
+  question: "Aksizli mallar üzərində vergi nəzarəti məqsədilə yaradılmış nəzarət postunun işçi-ləri satışa buraxılan hazır məhsulların sənədlərində göstərilən sayının, həcminin, çəkisinin onların faktiki sayı, həcm və çəkisi ilə düzgünlüyünü yoxlamalı və sənədlərdəki göstəriciləri harada qeyd etməlidirlər?",
+  answer: "C) Xüsusi jurnalda ",
+  options: [
+    "A) Aktda",
+    "B) Protokolda",
+    "C) Xüsusi jurnalda",
+    "D) Heç bir yerdə qeyd olunmur"
+  ]
+},
+{
+  numb: 39,
+  question: "Aksizli mallar üzərində vergi nəzarəti məqsədilə yaradılmış nəzarət postunun işçi-ləri “buraxılışa icazə verilir” qeydini hansı sənədlərdə aparmalıdırlar?",
+  answer: "A)	Malların ilkin təsdiqedici sənədlərində ",
+  options: [
+    "A)	Malların ilkin təsdiqedici sənədlərində ",
+    "B)	Heç bir qeyd aparılmır",
+    "C) Protokollarda",
+    "D) Xüsusi jurnalda"
+  ]
+},
+{
+  numb: 40,
+  question: "Plombların vurulması və çıxarılması arasındakı müddətdə nəzarət-ölçü cihazları-nın göstəricilərində dəyişikliklər edildikdə,texnoloji avadanhqlardakı plombların zədələnmə¬si halları müəyyən edildikdə nəzarət postnun işçiləri hansı tədbirləri həyata keçirməlidirlər?",
+  answer: "C)	Müvafiq akt tərtib etməli, bu barədə vergi orqanına məlumat verməlidirlər",
+  options: [
+    "A)	Dərhal hüquq mühafizə orqanlarına müraciət etməlidirlər",
+    "B)	Ancaq istehsalçını bu barədə məlumatlandırmalıdırlar",
+    "C)	Müvafiq akt tərtib etməli, bu barədə vergi orqanına məlumat verməlidirlər",
+    "D)	Şahidlərin iştirakı ilə protokol tərtib edilməlidir"
+  ]
+},
+{
+  numb: 41,
+  question: "Plombların vurulması və çıxarılması arasındakı müddətdə istehsal olunmuş məhsulların aksiz markası ilə markalanmasında, tam uçota alınmasında, aksiz markalarının inventarizasiyasında kənarlaşma edildikdə nəzarət postunun işçiləri hansı tədirləri həyata keçirməlidirlər?",
+  answer: "C)	Müvafiq akt tərtib etməli, bu barədə vergi orqanına məlumat verməlidirlər",
+  options: [
+    "A)	Dərhal hüquq mühafizə orqanlarına müraciət etməlidirlər",
+    "B)	Ancaq istehsalçını bu barədə məlumatlandırmalıdırlar",
+    "C)	Müvafiq akt tərtib etməli, bu barədə vergi orqanına məlumat verməlidirlər",
+    "D)	Şahidlərin iştirakı ilə protokol tərtib edilməlidir"
   ]
 },
 ];
