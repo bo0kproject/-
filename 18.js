783a1a6574b6afb6127696515fb29a0637d92e1c
bo0kproject <143459131+bo0kproject@users.noreply.github.com>
2023-09-18 12:39:04 +0400

    Update 18.js

--- 18.js
+++ 18.js
@@ -2,51 +2,73 @@
 let questions = [
   {
   numb: 1,
-  question: "Вопрос номер 1?",
-  answer: "1",
+  question: "Aksiz nədir?",
+  answer: "C)	Aksizli malların satış qiymətinə daxil edilən vergidir",
   options: [
-    "1",
-    "2",
-    "3",
-    "4"
+    "A)	Aksizli malların gəlirdən çıxılmayan xərclərinə aid edilməyən və müəssisənin mənfə-ətindən ödənilən vergidir",
+    "B)	Aksizli malların idxalına görə ödənilən xüsusi rüsumlardır",
+    "C)	Aksizli malların satış qiymətinə daxil edilən vergidir",
+    "D)	Aksizli malların maya dəyərinə daxil edilən vergidir"
   ]
 },
   {
   numb: 2,
-  question: "Вопрос номер 2?",
-  answer: "1",
+  question: "Hansı mallar istisna olunmaqla Azərbaycan Respublikasının ərazisində istehsal edi-lən, yaxud idxal olunan aksizli mallardan aksiz tutulur?",
+  answer: "A)	Vergidən azad edilən mallar",
   options: [
-    "1",
-    "2",
-    "3",
-    "4"
+    "A)	Vergidən azad edilən mallar",
+    "B)	ƏDV-yə sıfır (0) dərəcə ilə cəlb edilən mallar",
+    "C)	Vergi ödəyicisinin müəyyənləşdirdiyi mallar",
+    "D)	ƏDV-yə cəlb edilən mallar"
   ]
 },
   {
   numb: 3,
-  question: "Вопрос номер 3?",
-  answer: "1",
+  question: "Aşağıda göstərilən şəxslərdən hansı aksiz vergisinin ödəyicisi deyildir?",
+  answer: "D)	Aksizli malların satışı ilə məşğul olan şəxs",
   options: [
-    "1",
-    "2",
-    "3",
-    "4"
+    "A)	Aksizli malları istehsal edən müəssisələr",
+    "B)	Aksizli malları idxal edən şəxs",
+    "C)	Aksizli malları istehsal edən fiziki şəxs",
+    "D)	Aksizli malların satışı ilə məşğul olan şəxs"
   ]
 },
   {
   numb: 4,
-  question: "Вопрос номер 4?",
-  answer: "1",
+  question: "4.	Sİfarişçinin göndərdiyi xammaldan Azərbaycan Respublikası ərazisində mallar isteh¬sal edildiyi hallarda aksizin ödəyicisi kimdir?",
+  answer: "C)	Malların istehsalçısı [podratçı]",
   options: [
-    "1",
-    "2",
-    "3",
-    "4"
+    "A)	Sifarişçi",
+    "B)	Malların mülkiyyətçisi",
+    "C)	Malların istehsalçısı [podratçı]",
+    "D)	Malların son istehlakçıları"
   ]
 },
   {
   numb: 5,
-  question: "Вопрос номер 5?",
+  question: "Aksizli malların istehsalçısı və sifarişçisi qarşılıqlı surətdə asılı olan rezident şəxslər olduqda, aksizin ödəyicisi kimdir?",
+  answer: "D)	Malın sahibi (sifarişçi)",
+  options: [
+    "A)	Sifarişçi",
+    "B)	Malların mülkiyyətçisi",
+    "C)	Malların istehsalçısı [podratçı]",
+    "D)	Malın sahibi (sifarişçi)"
+  ]
+},
+{
+  numb: 6,
+  question: "Aşağıdakı əməliyyatlardan hansı aksiz vergisinin məqsədləri üçün vergitutma obyek-tidir?",
+  answer: "C)	Azərbaycan Respublikasının ərazisində istehsal edilən aksizli malların istehsal binasının hüdudlarından kənara buraxılması",
+  options: [
+    "A)	Malların Azərbaycan Respublikası ərazisinə müvəqqəti idxalı",
+    "B)	Tranzitlə daşman malların təkrar ixracı",
+    "C)	Azərbaycan Respublikasının ərazisində istehsal edilən aksizli malların istehsal binasının hüdudlarından kənara buraxılması",
+    "D)	Tranzitlə daşınan malların ixracı"
+  ]
+},
+  {
+  numb: 6,
+  question: "?",
   answer: "1",
   options: [
     "1",
@@ -57,7 +79,359 @@ let questions = [
 },
 {
   numb: 6,
-  question: "Вопрос номер 1?",
+  question: "?",
+  answer: "1",
+  options: [
+    "1",
+    "2",
+    "3",
+    "4"
+  ]
+},
+{
+  numb: 6,
+  question: "?",
+  answer: "1",
+  options: [
+    "1",
+    "2",
+    "3",
+    "4"
+  ]
+},
+{
+  numb: 6,
+  question: "?",
+  answer: "1",
+  options: [
+    "1",
+    "2",
+    "3",
+    "4"
+  ]
+},
+{
+  numb: 6,
+  question: "?",
+  answer: "1",
+  options: [
+    "1",
+    "2",
+    "3",
+    "4"
+  ]
+},
+{
+  numb: 6,
+  question: "?",
+  answer: "1",
+  options: [
+    "1",
+    "2",
+    "3",
+    "4"
+  ]
+},
+{
+  numb: 6,
+  question: "?",
+  answer: "1",
+  options: [
+    "1",
+    "2",
+    "3",
+    "4"
+  ]
+},
+{
+  numb: 6,
+  question: "?",
+  answer: "1",
+  options: [
+    "1",
+    "2",
+    "3",
+    "4"
+  ]
+},
+{
+  numb: 6,
+  question: "?",
+  answer: "1",
+  options: [
+    "1",
+    "2",
+    "3",
+    "4"
+  ]
+},
+{
+  numb: 6,
+  question: "?",
+  answer: "1",
+  options: [
+    "1",
+    "2",
+    "3",
+    "4"
+  ]
+},
+{
+  numb: 6,
+  question: "?",
+  answer: "1",
+  options: [
+    "1",
+    "2",
+    "3",
+    "4"
+  ]
+},
+{
+  numb: 6,
+  question: "?",
+  answer: "1",
+  options: [
+    "1",
+    "2",
+    "3",
+    "4"
+  ]
+},
+{
+  numb: 6,
+  question: "?",
+  answer: "1",
+  options: [
+    "1",
+    "2",
+    "3",
+    "4"
+  ]
+},
+{
+  numb: 6,
+  question: "?",
+  answer: "1",
+  options: [
+    "1",
+    "2",
+    "3",
+    "4"
+  ]
+},
+{
+  numb: 6,
+  question: "?",
+  answer: "1",
+  options: [
+    "1",
+    "2",
+    "3",
+    "4"
+  ]
+},
+{
+  numb: 6,
+  question: "?",
+  answer: "1",
+  options: [
+    "1",
+    "2",
+    "3",
+    "4"
+  ]
+},
+{
+  numb: 6,
+  question: "?",
+  answer: "1",
+  options: [
+    "1",
+    "2",
+    "3",
+    "4"
+  ]
+},
+{
+  numb: 6,
+  question: "?",
+  answer: "1",
+  options: [
+    "1",
+    "2",
+    "3",
+    "4"
+  ]
+},
+{
+  numb: 6,
+  question: "?",
+  answer: "1",
+  options: [
+    "1",
+    "2",
+    "3",
+    "4"
+  ]
+},
+{
+  numb: 6,
+  question: "?",
+  answer: "1",
+  options: [
+    "1",
+    "2",
+    "3",
+    "4"
+  ]
+},
+{
+  numb: 6,
+  question: "?",
+  answer: "1",
+  options: [
+    "1",
+    "2",
+    "3",
+    "4"
+  ]
+},
+{
+  numb: 6,
+  question: "?",
+  answer: "1",
+  options: [
+    "1",
+    "2",
+    "3",
+    "4"
+  ]
+},
+{
+  numb: 6,
+  question: "?",
+  answer: "1",
+  options: [
+    "1",
+    "2",
+    "3",
+    "4"
+  ]
+},
+{
+  numb: 6,
+  question: "?",
+  answer: "1",
+  options: [
+    "1",
+    "2",
+    "3",
+    "4"
+  ]
+},
+{
+  numb: 6,
+  question: "?",
+  answer: "1",
+  options: [
+    "1",
+    "2",
+    "3",
+    "4"
+  ]
+},
+{
+  numb: 6,
+  question: "?",
+  answer: "1",
+  options: [
+    "1",
+    "2",
+    "3",
+    "4"
+  ]
+},
+{
+  numb: 6,
+  question: "?",
+  answer: "1",
+  options: [
+    "1",
+    "2",
+    "3",
+    "4"
+  ]
+},
+{
+  numb: 6,
+  question: "?",
+  answer: "1",
+  options: [
+    "1",
+    "2",
+    "3",
+    "4"
+  ]
+},
+{
+  numb: 6,
+  question: "?",
+  answer: "1",
+  options: [
+    "1",
+    "2",
+    "3",
+    "4"
+  ]
+},
+{
+  numb: 6,
+  question: "?",
+  answer: "1",
+  options: [
+    "1",
+    "2",
+    "3",
+    "4"
+  ]
+},
+{
+  numb: 6,
+  question: "?",
+  answer: "1",
+  options: [
+    "1",
+    "2",
+    "3",
+    "4"
+  ]
+},
+{
+  numb: 6,
+  question: "?",
+  answer: "1",
+  options: [
+    "1",
+    "2",
+    "3",
+    "4"
+  ]
+},
+{
+  numb: 6,
+  question: "?",
+  answer: "1",
+  options: [
+    "1",
+    "2",
+    "3",
+    "4"
+  ]
+},
+{
+  numb: 6,
+  question: "?",
   answer: "1",
   options: [
     "1",
