7cefa0b62ba4c6479719bf5081e7a319d077b406
bo0kproject <143459131+bo0kproject@users.noreply.github.com>
2023-09-19 15:29:09 +0400

    Update 24.js

--- 24.js
+++ 24.js
@@ -2,35 +2,35 @@
 let questions = [
   {
   numb: 1,
-  question: "Вопрос номер 1?",
-  answer: "1",
+  question: "Rayon tabeliyində olan şəhərlərdə, qəsəbələrdə və kəndlərdə (Bakı və Sumqayıt şəhərlərinin, habelə Abşeron rayonunun qəsəbə, kəndləri istisna olmaqla) yerləşən yaşayış fondlarının, həyətyanı sahələrinin torpaqları və vətəndaşların bağ sahələrinin tutduğu torpaqlar üzrə 10000 m2- dən yuxarı olan hissə üçün, torpaq sahəsinin hər too kvadrat metrinə görə torpaq vergisinin dərəcəsi nə qədər təşkil edir?",
+  answer: "A) 0,2 manat",
   options: [
-    "1",
-    "2",
-    "3",
-    "4"
+    "A) 0,2 manat",
+    "B) 2 manat",
+    "C) 0,1 manat",
+    "D) 0,3 manat"
   ]
 },
   {
   numb: 2,
-  question: "Вопрос номер 2?",
-  answer: "1",
+  question: "Yaşayış fondlarının tutduğu torpaqların torpaq sahəsi 10000 m2- dən yuxan ol¬duqda, torpaq sahəsinin hər 100 kvadrat metrinə görə Bakı şəhəri üçün torpaq vergisinin dərəcəsi nə qədər təşkil edir?",
+  answer: "D) 1,2 manat",
   options: [
-    "1",
-    "2",
-    "3",
-    "4"
+    "A) 0,5 manat",
+    "B) 0,4 manat",
+    "C) 0,6 manat",
+    "D) 1,2 manat"
   ]
 },
   {
   numb: 3,
-  question: "Вопрос номер 3?",
-  answer: "1",
+  question: "Aşağıdakı torpaqlardan hansılar torpaq vergisinə cəlb edilmir? 1)	yaşayış məntəqələrinin ümumi istifadədə olan torpaqları; 2)	Mərkəzi Bankının və onun qurumlarının mülkiyyətində və istifadəsində olan torpaqlar 3)	yaşayış fondlarının torpaqları; 4)	fiziki və hüquqi şəxslərin istifadəsinə verilməmiş dövlət, meşə və su fondu torpaqları; 5)	vətəndaşların bağ sahələrinin tutduğu torpaqlar. 6)	dövlətin adından yaradılan publik hüquqi şəxslərin (sahibkarlıq fəaliyyəti məqsədləri üçün istifadə olunan torpaqlar istisna olmaqla)",
+  answer: "A) l;2;4;6",
   options: [
-    "1",
-    "2",
-    "3",
-    "4"
+    "A) l;2;4;6",
+    "B)  l;2;5",
+    "C)2;3;4;6",
+    "D) 3;4;5;6"
   ]
 },
   {
@@ -46,7 +46,18 @@ let questions = [
 },
   {
   numb: 5,
-  question: "Вопрос номер 5?",
+  question: "Aşağıdakı torpaqlardan hansılar torpaq vergisinə cəlb edilir? 1)	dövlət sərhəd zolaqları və müdafiə təyinatlı torpaqlar 2)	həyətyanı sahələrin torpaqları 3)	yaşayış fondlarının torpaqları 4)	tikinti, nəqliyyat, rabitə, ticarət-məişət xidməti təyinatlı torpaqlar 5)	Xəzər dənizinin [gülünün]Azərbaycana məxsus olan sektorunun altında yerləşən torpaqlar",
+  answer: "A) 2;3;4",
+  options: [
+    "A) 2;3;4",
+    "В) 1;2;4",
+    "C) 3;4;5",
+    "D) l;3;5"
+  ]
+},
+{
+  numb: 6,
+  question: "Вопрос номер 1?",
   answer: "1",
   options: [
     "1",
@@ -55,9 +66,273 @@ let questions = [
     "4"
   ]
 },
-{
-  numb: 6,
-  question: "Вопрос номер 1?",
+  {
+  numb: 1,
+  question: "Воп",
+  answer: "1",
+  options: [
+    "1",
+    "2",
+    "3",
+    "4"
+  ]
+},
+  {
+  numb: 1,
+  question: "Воп",
+  answer: "1",
+  options: [
+    "1",
+    "2",
+    "3",
+    "4"
+  ]
+},
+  {
+  numb: 1,
+  question: "Воп",
+  answer: "1",
+  options: [
+    "1",
+    "2",
+    "3",
+    "4"
+  ]
+},
+  {
+  numb: 1,
+  question: "Воп",
+  answer: "1",
+  options: [
+    "1",
+    "2",
+    "3",
+    "4"
+  ]
+},
+  {
+  numb: 1,
+  question: "Воп",
+  answer: "1",
+  options: [
+    "1",
+    "2",
+    "3",
+    "4"
+  ]
+},
+  {
+  numb: 1,
+  question: "Воп",
+  answer: "1",
+  options: [
+    "1",
+    "2",
+    "3",
+    "4"
+  ]
+},
+  {
+  numb: 1,
+  question: "Воп",
+  answer: "1",
+  options: [
+    "1",
+    "2",
+    "3",
+    "4"
+  ]
+},
+  {
+  numb: 1,
+  question: "Воп",
+  answer: "1",
+  options: [
+    "1",
+    "2",
+    "3",
+    "4"
+  ]
+},
+  {
+  numb: 1,
+  question: "Воп",
+  answer: "1",
+  options: [
+    "1",
+    "2",
+    "3",
+    "4"
+  ]
+},
+  {
+  numb: 1,
+  question: "Воп",
+  answer: "1",
+  options: [
+    "1",
+    "2",
+    "3",
+    "4"
+  ]
+},
+  {
+  numb: 1,
+  question: "Воп",
+  answer: "1",
+  options: [
+    "1",
+    "2",
+    "3",
+    "4"
+  ]
+},
+  {
+  numb: 1,
+  question: "Воп",
+  answer: "1",
+  options: [
+    "1",
+    "2",
+    "3",
+    "4"
+  ]
+},
+  {
+  numb: 1,
+  question: "Воп",
+  answer: "1",
+  options: [
+    "1",
+    "2",
+    "3",
+    "4"
+  ]
+},
+  {
+  numb: 1,
+  question: "Воп",
+  answer: "1",
+  options: [
+    "1",
+    "2",
+    "3",
+    "4"
+  ]
+},
+  {
+  numb: 1,
+  question: "Воп",
+  answer: "1",
+  options: [
+    "1",
+    "2",
+    "3",
+    "4"
+  ]
+},
+  {
+  numb: 1,
+  question: "Воп",
+  answer: "1",
+  options: [
+    "1",
+    "2",
+    "3",
+    "4"
+  ]
+},
+  {
+  numb: 1,
+  question: "Воп",
+  answer: "1",
+  options: [
+    "1",
+    "2",
+    "3",
+    "4"
+  ]
+},
+  {
+  numb: 1,
+  question: "Воп",
+  answer: "1",
+  options: [
+    "1",
+    "2",
+    "3",
+    "4"
+  ]
+},
+  {
+  numb: 1,
+  question: "Воп",
+  answer: "1",
+  options: [
+    "1",
+    "2",
+    "3",
+    "4"
+  ]
+},
+  {
+  numb: 1,
+  question: "Воп",
+  answer: "1",
+  options: [
+    "1",
+    "2",
+    "3",
+    "4"
+  ]
+},
+  {
+  numb: 1,
+  question: "Воп",
+  answer: "1",
+  options: [
+    "1",
+    "2",
+    "3",
+    "4"
+  ]
+},
+  {
+  numb: 1,
+  question: "Воп",
+  answer: "1",
+  options: [
+    "1",
+    "2",
+    "3",
+    "4"
+  ]
+},
+  {
+  numb: 1,
+  question: "Воп",
+  answer: "1",
+  options: [
+    "1",
+    "2",
+    "3",
+    "4"
+  ]
+},
+  {
+  numb: 1,
+  question: "Воп",
+  answer: "1",
+  options: [
+    "1",
+    "2",
+    "3",
+    "4"
+  ]
+},
+  {
+  numb: 1,
+  question: "Воп",
   answer: "1",
   options: [
     "1",
