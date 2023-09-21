63e2867a781df32de2eaa1fc948c3d1d424ae73a
bo0kproject <143459131+bo0kproject@users.noreply.github.com>
2023-09-21 13:28:56 +0400

    Update 27.js

--- 27.js
+++ 27.js
@@ -5,8 +5,8 @@ let questions = [
     question: "ƏDV məqsədləri üçün qeydiyyata alınmamış və ardıcıl on iki aylıq dövrün istənilən ayında (aylarında) vergi tutulan əməliyyatlarının həcmi ən azı hansı məbləğlərdən az olan hüquqi və fiziki şəxslər sadələşdirilmiş vergi ödəyicisi ola bilərlər?",
     answer: "A)	200000 manat və ondan az",
     options: [
-      "A)	200000 manat və ondan az",
-      "B)	210000 manat və ondan az",
+      "A) 200000 manat və ondan az",
+      "B) 210000 manat və ondan az",
       "C) 150000 manat və ondan az",
       "D) 90000 manat və ondan az"
     ]
@@ -155,168 +155,167 @@ let questions = [
     ]
   },
   {
-    numb: 1,
-    question: "",
-    answer: "1",
+    numb: 15,
+    question: "Aksizli mallar istehsal edən şəxslər nə vaxt sadələşdirilmiş verginin ödəyicisi ola bilərlər?",
+    answer: "A)	Heç vaxt ola bilməzlər",
     options: [
-      "1",
-      "2",
-      "2",
-      "3"
+      "A)	Heç vaxt ola bilməzlər",
+      "B)	Bu fəaliyyətlə yanaşı beynəlxalq yük və sərnişin daşımaları ilə məşğul olduqda",
+      "C)	Fəaliyyətini müvəqqəti dayandırdıqda",
+      "D)	Ardıcıl 12 aylıq dövrün istənilən ayında (aylarında) vergi tutulan əməliyyatların həcmi 200.000 manatdan az olduqda"
     ]
   },
   {
-    numb: 1,
-    question: "",
-    answer: "1",
+    numb: 16,
+    question: "Aşağıda göstərilənlərin hansının sadələşdirilmiş verg ödəyicisi olmaq hüququ vardır? 1)	sığorta təşkilatlarının 2) investisiya fondlarının 3)	kredit təşkilatlarının 4) lombardların",
+    answer: "D) Heç birinin",
     options: [
-      "1",
-      "2",
-      "2",
-      "3"
+      "A) 1,2",
+      "B) 1,3",
+      "C) 1,3,4",
+      "D) Heç birinin"
     ]
   },
   {
-    numb: 1,
-    question: "",
-    answer: "1",
+    numb: 17,
+    question: "Kredit təşkilatları nə vaxt sadələşdirilmiş verginin ödəyicisi ola bilərlər?",
+    answer: "D)	Heç vaxt ola bilməzlər",
     options: [
-      "1",
-      "2",
-      "2",
-      "3"
+      "A)	Ardıcıl 12 aylıq dövrün istənilən ayında (aylarında) vergi tutulan əməliyyatların həcmi 200.000 manatdan az olduqda",
+      "B)	Əmlakın icarəyə verilməsindən gəlir əldə etdikdə",
+      "C)	Bu fəaliyyətlə yanaşı lombard fəaliyyəti ilə məşğul olduqda",
+      "D)	Heç vaxt ola bilməzlər"
     ]
   },
   {
-    numb: 1,
-    question: "",
-    answer: "1",
+    numb: 18,
+    question: "İcbari sığorta müqavilələri üzrə xidmət göstərən sığorta agentləri bu fəaliyyətlə yanaşı digər fəaliyyət növləri ilə də məşğul olduqda, həmin müqavilələr üzrə ümumi hasilata sadələşdirilmiş vergini tətbiq edə bilərlərmi?",
+    answer: "B)	Edə bilərlər",
     options: [
-      "1",
-      "2",
-      "2",
-      "3"
+      "A)	Edə bilməzlər",
+      "B)	Edə bilərlər",
+      "C)	İcbari sığorta müqavilələri üzrə xidmətlərdən əldə olunan vəsaitin 50 faizinə sadələş¬dirilmiş vergi tətbiq olunur.",
+      "D)	İcbari sığorta müqavilələri üzrə xidmətlərdən əldə olunan vəsaitin 75 faizinə sadələş¬dirilmiş vergi tətbiq olunur."
     ]
   },
   {
-    numb: 1,
-    question: "",
-    answer: "1",
+    numb: 19,
+    question: "Qeyri-dövlət pensiya fondları nə zaman sadələşdirilmiş vergi ödəyicisi ola bilərlər?",
+    answer: "A)	Heç vaxt ola bilməzlər",
     options: [
-      "1",
-      "2",
-      "2",
-      "3"
+      "A)	Heç vaxt ola bilməzlər",
+      "B)	Mülkiyyətində əsas vəsaitlərin ilin əvvəlinə qalıq dəyəri 1000.000 manatdan az olarsa, istədiyi zaman",
+      "C)	Mülkiyyətində olan əsas vəsaitlərin ilin əvvəlinə qalıq dəyəri 1000.000 manatdan artıq olarsa, istədiyi zaman",
+      "D)	Hər bir halda"
     ]
   },
   {
-    numb: 1,
-    question: "",
-    answer: "1",
+    numb: 20,
+    question: "Əmlakın icarəyə verilməsindən və royaltidən gəlir əldə edən şəxslər nə zaman sadələşdirilmiş vergi ödəyicisi ola bilərlər?",
+    answer: "D)	Bu fəaliyyətlə yanaşı digər fəaliyyətlə məşğul olduqda digər fəaliyyət üzrə",
     options: [
-      "1",
-      "2",
-      "2",
-      "3"
+      "A)	Mülkiyyət, olan əsas vəsaitlərin ilin əvvəlinə qalıq dəyəri 1000.000 man-dan az olduqda",
+      "B)	Ardıcıl 12 aylıq dövrün istənilən ayında (aylarında) vergi tutulan əməliyyatların həcmi 200.000 manatdan az olduqda",
+      "C)	Aksizli mallar istehsal etdikdə",
+      "D)	Bu fəaliyyətlə yanaşı digər fəaliyyətlə məşğul olduqda digər fəaliyyət üzrə"
     ]
   },
   {
-    numb: 1,
-    question: "",
-    answer: "1",
+    numb: 21,
+    question: "Aşağıda göstərilən şəxslərdən hansı sadələşdirilmiş verginin ödəyicisi deyil?",
+    answer: "D)	Müvafiq icra hakimiyyəti orqanı tərəfindən müəyyən edilmiş təbii inhisar subyektləri",
     options: [
-      "1",
-      "2",
-      "2",
-      "3"
+      "A)	Mülkiyyətində və ya istifadəsində olan avtonəqliyyat vasitələri ilə (beynəlxalq yük və sərnişin daşımaları istisna olmaqla) Azərbaycan Resp. ərazisində sərnişin və yük daşımalarını (o cümlədən taksi ilə) müqavilə əsasında digər şəxslər vasitəsilə həyata keçirən şəxslər",
+      "B)	Ardıcıl 12 aylıq dövrün istənilən ayında (aylarında) vergi tutulan əməliyyatların həcmi 200.000 manatdan artıq olan ictimai iaşə fəaliyyəti ilə məşğul olan şəxslər",
+      "C)	İdman mərc oyunlarının operatoru və satıcıları olan şəxslər",
+      "D)	Müvafiq icra hakimiyyəti orqanı tərəfindən müəyyən edilmiş təbii inhisar subyektləri"
     ]
   },
   {
-    numb: 1,
-    question: "",
-    answer: "1",
+    numb: 22,
+    question: "Vergi Məcəlləsinin 218.5.9-cu maddəsinin müddəalarından asılı olmayaraq pərakəndə satış qaydasında malların təqdim edilməsini həyata keçirən vergi ödəyicisi eyni zamanda topdansatış qaydasında malların satışını həyata keçirdikdə sadələşdirilmiş vergi metodunu tətbiq etmək hüququ varmı?",
+    answer: "C)	Rüb ərzində elektron qaimə-faktura ilə rəsmiləşdirilməli olan əməliyyatların həcmi ümumi ticarət əməliyyatlarının (satışdankənar gəlirlər istisna olmaqla) həcminin 30 faizindən çox olmadıqda sadələşdirilmiş vergi ödəyicisi olmaq hüququ vardır",
     options: [
-      "1",
-      "2",
-      "2",
-      "3"
+      "A) Yoxdur",
+      "B)	Vardır",
+      "C)	Rüb ərzində elektron qaimə-faktura ilə rəsmiləşdirilməli olan əməliyyatların həcmi ümumi ticarət əməliyyatlarının (satışdankənar gəlirlər istisna olmaqla) həcminin 30 faizindən çox olmadıqda sadələşdirilmiş vergi ödəyicisi olmaq hüququ vardır",
+      "D)	Rüb ərzində elektron qaimə-faktura ilə rəsmiləşdirilməli olan əməliyyatların həcmi ümumi ticarət əməliyyatlarının (satışdankənar gəlirlər də daxil olmaqla) həcminin 30 faizindən çox olmadıqda sadələşdirilmiş vergi ödəyicisi olmaq hüququ vardır"
     ]
   },
   {
-    numb: 1,
-    question: "",
-    answer: "1",
+    numb: 23,
+    question: "23.	Vergi Məcəlləsinin 218.5.10-cu maddəsinin müddəalarından asılı olmayaraq Vergi ödəyicisi kimi vergi orqanında uçotda olmayan şəxslərə (əhaliyə) göstərilən xidmətlərdən başqa, xidmətlərin göstərilməsini həyata keçirən şəxslər tərəfindən əhaliyə xidmətlə yanaşı hüquqi şəxslərə və vergi ödəyicisi kimi vergi orqanında uçotda olan fiziki şəxslərə xidmətlərin göstərilməsi, hansı halda sadələşdirilmiş vergi metodunu tətbiq etmək hüququ olur?",
+    answer: "D)	Rüb ərzində elektron qaimə-faktura ilə rəsmiləşdirilməli olan əməliyyatların həcmi xidmətlərin göstərilməsi üzrə ümumi əməliyyatların (satışdankənar gəlirlər istisna olmaqla) həcminin 30 faizindən çox olmadıqda",
     options: [
-      "1",
-      "2",
-      "2",
-      "3"
+      "A)	Rüb ərzində elektron qaimə-faktura ilə rəsmiləşdirilməli olan əməliyyatların həcmi xidmətlərin göstərilməsi üzrə ümumi əməliyyatların (satışdankənar gəlirlər də daxil olmaqla) həcminin 30 faizindən çox olmadıqda",
+      "C)	Rüb ərzində elektron qaimə-faktura ilə rəsmiləşdirilməli olan əməliyyatların həcmi xidmətlərin göstərilməsi üzrə ümumi əməliyyatların (satışdankənar gəlirlər istisna olmaqla) həcminin 30 faizindən çox olduqda",
+      "D)	Rüb ərzində elektron qaimə-faktura ilə rəsmiləşdirilməli olan əməliyyatların həcmi xidmətlərin göstərilməsi üzrə ümumi əməliyyatların (satışdankənar gəlirlər istisna olmaqla) həcminin 30 faizindən çox olmadıqda"
     ]
   },
   {
-    numb: 1,
-    question: "",
-    answer: "1",
+    numb: 24,
+    question: "Kənd təsərrüfatı məhsullarının istehsalçıları (o cümlədən sənaye üsulu ilə) tərəfindən özlərinin istehsal etdikləri kənd təsərrüfatı məhsullarının satışından əldə edilmiş hasilatın həcmi sadələşdirilmiş vergiyə necə cəlb olunur?",
+    answer: "A)	2014-cü il yanvarın 1-dən 10 il müddətinə vergidən azaddır",
     options: [
-      "1",
-      "2",
-      "2",
-      "3"
+      "A)	2014-cü il yanvarın 1-dən 10 il müddətinə vergidən azaddır",
+      "B)	Müddətsiz vergidən azaddır",
+      "C)	2014-cü il yanvarın 1-dən 5 il müddətinə vergidən azaddır",
+      "D)	2 faiz dərəcəsi ilə"
     ]
   },
   {
-    numb: 1,
-    question: "",
-    answer: "1",
+    numb: 25,
+    question: "Qanunla müəyyən edilmiş hallarda dövlət büdcəsinə olan vergi borclarının silinməsindən əldə olunan gəlir sadələşdirilmiş vergiyə necə cəlb olunur?",
+    answer: "B)	Müddətsiz vergidən azaddır",
     options: [
-      "1",
-      "2",
-      "2",
-      "3"
+      "A)	2020-ci il yanvarın 1-dən 7 il müddətinə vergidən azaddır",
+      "B)	Müddətsiz vergidən azaddır",
+      "C)	2014-cü il yanvarın 1-dən 5 il müddətinə vergidən azaddır",
+      "D)	2 faiz dərəcəsi ilə"
     ]
   },
   {
-    numb: 1,
-    question: "",
-    answer: "1",
+    numb: 26,
+    question: "Müvafiq icra hakimiyyəti orqanının müəyyən etdiyi orqan (qurum) tərəfindən müəyyən olunmuş qaydada dövlət büdcəsinin vəsaiti hesabına ödənilən ixrac təşviqi hansı qaydada sadələşdirilmiş vergiyə cəlb olunur?",
+    answer: "A)	Müddətsiz vergidən azaddır",
     options: [
-      "1",
-      "2",
-      "2",
-      "3"
+      "A)	Müddətsiz vergidən azaddır",
+      "B)	2020-ci il yanvarın 1-dən 7 il müddətinə vergidən azaddır",
+      "C)	2014-cü il yanvarın 1-dən 5 il müddətinə vergidən azaddır",
+      "D)	2 faiz dərəcəsi ilə"
     ]
   },
   {
-    numb: 1,
-    question: "",
-    answer: "1",
+    numb: 27,
+    question: "Vergi Məcəlləsi ilə müəyyən olunmuş qaydada gəlirlərin və xərclərin uçotunu aparan, ƏDV-nin məqsədləri üçün qeydiyyata alınmayan və əməliyyatlarının həcmi ardıcıl 12 aylıq dövrün istənilən ayında (aylarında) 200.000 manatadək olan rezident müəssisənin təsisçisi (payçısı) və yaxud səhmdarları olan şəxslərə ödənilən dividendlərdən sadələşdirilmiş vergi hansı dərəcə ilə hesablanır?",
+    answer: "B) Vergidən azaddır",
     options: [
-      "1",
-      "2",
-      "2",
-      "3"
+      "A) 10 faiz dərəcəsi ilə",
+      "B) Vergidən azaddır",
+      "C) 4 faiz dərəcəsi ilə",
+      "D) 2 faiz dərəcəsi ilə"
     ]
   },
   {
-    numb: 1,
-    question: "",
-    answer: "1",
+    numb: 28,
+    question: "Fiziki şəxslər tərəfindən azı 3 təqvim ili ərzində yaşayış yeri üzrə qeydiyyatda olduğu yaşayış sahələrinin təqdim edilməsindən sadələşdirilmiş vergi necə tutulur?",
+    answer: "B) Vergidən azaddır",
     options: [
-      "1",
-      "2",
-      "2",
-      "3"
+      "A) 10 faiz dərəcəsi ilə",
+      "B) Vergidən azaddır",
+      "C) 4 faiz dərəcəsi ilə",
+      "D) 2 faiz dərəcəsi ilə"
     ]
   },
   {
-    numb: 1,
-    question: "",
-    answer: "1",
+    numb: 29,
+    question: "Fiziki şəxs sökülmüş binada yerləşən yaşayış sahəsində azı 3 təqvim ili ərzində (sökülmüş və sökülmüş binanın yerində tikilən yeni binada yaşayış yeri üzrə qeydiyyatda olduğu müddətlər cəmlənməklə) yaşayış yeri üzrə qeydiyyatda olduğu halda, sökülmüş binanın yerində tikilən yeni binadan həmin fiziki şəxsə verilən yaşayış sahəsinin təqdim edilməsindən sadələşdirilmiş vergi necə tutulur?",
+    answer: "B) Vergidən azaddır",
     options: [
-      "1",
-      "2",
-      "2",
-      "3"
+      "A) 10 faiz dərəcəsi ilə",
+      "B) Vergidən azaddır",
+      "C) 4 faiz dərəcəsi ilə",
+      "D) 2 faiz dərəcəsi ilə"
     ]
   },
   {
