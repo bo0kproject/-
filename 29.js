c93fa01b6a8899cfe07e165b68608868271c1c29
bo0kproject <143459131+bo0kproject@users.noreply.github.com>
2023-09-21 06:44:21 +0400

    Create 29.js

---
+++ 29.js
@@ -0,0 +1,520 @@
+// создание вопросов и ответов
+let questions = [
+    {
+    numb: 1,
+    question: "",
+    answer: "A)",
+    options: [
+      "A)",
+      "B)",
+      "C)",
+      "D)"
+    ]
+  },
+  {
+    numb: 2,
+    question: "",
+    answer: "1",
+    options: [
+      "1",
+      "2",
+      "2",
+      "3"
+    ]
+  },
+  {
+    numb: 1,
+    question: "",
+    answer: "1",
+    options: [
+      "1",
+      "2",
+      "2",
+      "3"
+    ]
+  },
+  {
+    numb: 1,
+    question: "",
+    answer: "1",
+    options: [
+      "1",
+      "2",
+      "2",
+      "3"
+    ]
+  },
+  {
+    numb: 1,
+    question: "",
+    answer: "1",
+    options: [
+      "1",
+      "2",
+      "2",
+      "3"
+    ]
+  },
+  {
+    numb: 1,
+    question: "",
+    answer: "1",
+    options: [
+      "1",
+      "2",
+      "2",
+      "3"
+    ]
+  },
+  {
+    numb: 1,
+    question: "",
+    answer: "1",
+    options: [
+      "1",
+      "2",
+      "2",
+      "3"
+    ]
+  },
+  {
+    numb: 1,
+    question: "",
+    answer: "1",
+    options: [
+      "1",
+      "2",
+      "2",
+      "3"
+    ]
+  },
+  {
+    numb: 1,
+    question: "",
+    answer: "1",
+    options: [
+      "1",
+      "2",
+      "2",
+      "3"
+    ]
+  },
+  {
+    numb: 1,
+    question: "",
+    answer: "1",
+    options: [
+      "1",
+      "2",
+      "2",
+      "3"
+    ]
+  },
+  {
+    numb: 1,
+    question: "",
+    answer: "1",
+    options: [
+      "1",
+      "2",
+      "2",
+      "3"
+    ]
+  },
+  {
+    numb: 1,
+    question: "",
+    answer: "1",
+    options: [
+      "1",
+      "2",
+      "2",
+      "3"
+    ]
+  },
+  {
+    numb: 1,
+    question: "",
+    answer: "1",
+    options: [
+      "1",
+      "2",
+      "2",
+      "3"
+    ]
+  },
+  {
+    numb: 1,
+    question: "",
+    answer: "1",
+    options: [
+      "1",
+      "2",
+      "2",
+      "3"
+    ]
+  },
+  {
+    numb: 1,
+    question: "",
+    answer: "1",
+    options: [
+      "1",
+      "2",
+      "2",
+      "3"
+    ]
+  },
+  {
+    numb: 1,
+    question: "",
+    answer: "1",
+    options: [
+      "1",
+      "2",
+      "2",
+      "3"
+    ]
+  },
+  {
+    numb: 1,
+    question: "",
+    answer: "1",
+    options: [
+      "1",
+      "2",
+      "2",
+      "3"
+    ]
+  },
+  {
+    numb: 1,
+    question: "",
+    answer: "1",
+    options: [
+      "1",
+      "2",
+      "2",
+      "3"
+    ]
+  },
+  {
+    numb: 1,
+    question: "",
+    answer: "1",
+    options: [
+      "1",
+      "2",
+      "2",
+      "3"
+    ]
+  },
+  {
+    numb: 1,
+    question: "",
+    answer: "1",
+    options: [
+      "1",
+      "2",
+      "2",
+      "3"
+    ]
+  },
+  {
+    numb: 1,
+    question: "",
+    answer: "1",
+    options: [
+      "1",
+      "2",
+      "2",
+      "3"
+    ]
+  },
+  {
+    numb: 1,
+    question: "",
+    answer: "1",
+    options: [
+      "1",
+      "2",
+      "2",
+      "3"
+    ]
+  },
+  {
+    numb: 1,
+    question: "",
+    answer: "1",
+    options: [
+      "1",
+      "2",
+      "2",
+      "3"
+    ]
+  },
+  {
+    numb: 1,
+    question: "",
+    answer: "1",
+    options: [
+      "1",
+      "2",
+      "2",
+      "3"
+    ]
+  },
+  {
+    numb: 1,
+    question: "",
+    answer: "1",
+    options: [
+      "1",
+      "2",
+      "2",
+      "3"
+    ]
+  },
+  {
+    numb: 1,
+    question: "",
+    answer: "1",
+    options: [
+      "1",
+      "2",
+      "2",
+      "3"
+    ]
+  },
+  {
+    numb: 1,
+    question: "",
+    answer: "1",
+    options: [
+      "1",
+      "2",
+      "2",
+      "3"
+    ]
+  },
+  {
+    numb: 1,
+    question: "",
+    answer: "1",
+    options: [
+      "1",
+      "2",
+      "2",
+      "3"
+    ]
+  },
+  {
+    numb: 1,
+    question: "",
+    answer: "1",
+    options: [
+      "1",
+      "2",
+      "2",
+      "3"
+    ]
+  },
+  {
+    numb: 1,
+    question: "",
+    answer: "1",
+    options: [
+      "1",
+      "2",
+      "2",
+      "3"
+    ]
+  },
+  {
+    numb: 1,
+    question: "",
+    answer: "1",
+    options: [
+      "1",
+      "2",
+      "2",
+      "3"
+    ]
+  },
+  {
+    numb: 1,
+    question: "",
+    answer: "1",
+    options: [
+      "1",
+      "2",
+      "2",
+      "3"
+    ]
+  },
+  {
+    numb: 1,
+    question: "",
+    answer: "1",
+    options: [
+      "1",
+      "2",
+      "2",
+      "3"
+    ]
+  },
+  {
+    numb: 1,
+    question: "",
+    answer: "1",
+    options: [
+      "1",
+      "2",
+      "2",
+      "3"
+    ]
+  },
+  {
+    numb: 1,
+    question: "",
+    answer: "1",
+    options: [
+      "1",
+      "2",
+      "2",
+      "3"
+    ]
+  },
+  {
+    numb: 1,
+    question: "",
+    answer: "1",
+    options: [
+      "1",
+      "2",
+      "2",
+      "3"
+    ]
+  },
+  {
+    numb: 1,
+    question: "",
+    answer: "1",
+    options: [
+      "1",
+      "2",
+      "2",
+      "3"
+    ]
+  },
+  {
+    numb: 1,
+    question: "",
+    answer: "1",
+    options: [
+      "1",
+      "2",
+      "2",
+      "3"
+    ]
+  },
+  {
+    numb: 1,
+    question: "",
+    answer: "1",
+    options: [
+      "1",
+      "2",
+      "2",
+      "3"
+    ]
+  },
+  {
+    numb: 1,
+    question: "",
+    answer: "1",
+    options: [
+      "1",
+      "2",
+      "2",
+      "3"
+    ]
+  },
+  {
+    numb: 1,
+    question: "",
+    answer: "1",
+    options: [
+      "1",
+      "2",
+      "2",
+      "3"
+    ]
+  },
+  {
+    numb: 1,
+    question: "",
+    answer: "1",
+    options: [
+      "1",
+      "2",
+      "2",
+      "3"
+    ]
+  },
+  {
+    numb: 1,
+    question: "",
+    answer: "1",
+    options: [
+      "1",
+      "2",
+      "2",
+      "3"
+    ]
+  },
+  {
+    numb: 1,
+    question: "",
+    answer: "1",
+    options: [
+      "1",
+      "2",
+      "2",
+      "3"
+    ]
+  },
+  {
+    numb: 1,
+    question: "",
+    answer: "1",
+    options: [
+      "1",
+      "2",
+      "2",
+      "3"
+    ]
+  },
+  {
+    numb: 1,
+    question: "",
+    answer: "1",
+    options: [
+      "1",
+      "2",
+      "2",
+      "3"
+    ]
+  },
+  {
+    numb: 1,
+    question: "",
+    answer: "1",
+    options: [
+      "1",
+      "2",
+      "2",
+      "3"
+    ]
+  },
+  ];
